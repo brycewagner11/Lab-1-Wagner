d7d51b5003649ceced92997a0ef9eeb19eb13c5e
Bryce Wagner <bwagner28@rockets.utoledo.edu>
2024-04-02 16:38:47 -0400

    first commit

---
+++ Lab 1 Wagner/Laboratory.cpp
@@ -0,0 +1,23 @@
+/*
+* Program: <Lab1Wagner>
+*  Author: <Bryce Wagner>
+*  Date:<1/23/2024>
+* 
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+	cout << "Who's on First? \n";
+		return 0;
+	}
